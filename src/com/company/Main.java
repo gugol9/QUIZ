5672b7aad736db91397c21f0676e308fdac4ffef
Kamil Markowski <markian99@wp.pl>
2021-12-29 21:14:50 +0100

    1.04 nie dzialajaca tab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,26 @@ package com.company;
 import java.io.FileNotFoundException;
 
 public class Main {
+    static int N = 5;
+
     public static void main(String[] args) throws FileNotFoundException {
 
+        Question questions[] = new Question[N];
+       // questions[0] = new int(1);
 
-        Question a = new Question();
-        a.load();
-        a.ask();
-        a.check();
-        System.out.println("Koniec quizu zdobyte punkty: " + a.score);
+        //Question q = new Question();
+        int sum = 0;
+
+        for(int i = 0; i < questions.length ;i++)
+        {
+            questions[i].number_question = i + 1;
+            questions[i].load();
+            questions[i].ask();
+            questions[i].check();
+            sum+= questions[i].score;
+
+
+        }
+        System.out.println("Koniec quizu zdobyte punkty: " + sum);
     }
 }
--- src/com/company/Question.java
+++ src/com/company/Question.java
@@ -11,8 +11,8 @@ public class Question {
          String a, b, c, d;     //mozliwe odpowiedzi
          String true_answer;
          String user_answer;
-         int number_pytania;
-         int score;  // 1 or 0
+         int number_question;
+         int score;
 
     void load() throws FileNotFoundException     //this void reads data from a file FileNot... wyjatek
     {
@@ -20,10 +20,10 @@ public class Question {
         Scanner scanner = new Scanner(file);        //tworze nowy obiekt scanner klasy Scanner
 
        String line;                                        // przechowuje biezaca linie z pliku
-        int line_number = (number_pytania+1);//-1)*6+1;            //nr statrotwy lini od ktorej bedzimey czytac
+        int line_number = (number_question-1)*6+1;            //nr statrotwy lini od ktorej bedzimey czytac
         int current_line = 1;                               // aktualny nr lini
 
-// TU JEST PROBLEM
+
         while(scanner.hasNext())
        {
            line = scanner.nextLine();
@@ -44,19 +44,18 @@ public class Question {
 
     void ask() // asks a question
     {
-        Scanner scan = new Scanner(System.in);  //mozliwosc wprwoadzenia danych
+        Scanner scan = new Scanner(System.in);  //mozliwosc wprwoadzenia danych klasa Scanner
         System.out.println("Pytanie: "+contents);
         System.out.println("a) " + a);
         System.out.println("b) " + b);
         System.out.println("c) " + c);
         System.out.println("d) " + d);
         System.out.println("-------------");
-        System.out.println(true_answer);
          user_answer = scan.nextLine();         //wprowadzenie odp
     }
     void check()    // checks a answer
     {
-        if(Objects.equals(user_answer, true_answer)) // porownywanie stringow jednen rowny drugiemu 
+        if(Objects.equals(user_answer, true_answer)) // porownywanie stringow jednen rowny drugiemu
         {
             score = 1;
         }else score = 0;
